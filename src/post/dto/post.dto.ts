5bb5c282fffaf33b85decf07e72dab90f6eda834
devga <biennguyen131311@gmail.com>
2023-04-07 00:46:36 +0700

    #3

--- src/post/dto/post.dto.ts
+++ src/post/dto/post.dto.ts
@@ -1,13 +1,15 @@
 import { IsNotEmpty } from 'class-validator';
 
 export class CreatePostDto {
-  @IsNotEmpty() title: string;
+  @IsNotEmpty()
+  title: string;
   description: string;
   content: string;
 }
 
 export class UpdatePostDto {
-  @IsNotEmpty() title: string;
+  @IsNotEmpty()
+  title: string;
   description: string;
   content: string;
 }
--- src/post/post.module.ts
+++ src/post/post.module.ts
@@ -1,9 +1,20 @@
 import { Module } from '@nestjs/common';
 import { PostService } from './services/post.service';
 import { PostController } from './controllers/post.controller';
+import { MongooseModule } from '@nestjs/mongoose';
+import { PostSchema } from './models/post.model';
+import { PostRepository } from './repositories/post.repository';
 
 @Module({
-  providers: [PostService],
+  imports: [
+    MongooseModule.forFeature([
+      {
+        name: 'Post',
+        schema: PostSchema,
+      },
+    ]),
+  ],
+  providers: [PostService, PostModule, PostRepository],
   controllers: [PostController],
 })
 export class PostModule {}
